c4801720ddc4c69e18e13d493d883c8dc2b7cb6b
Vaibhav Meena <vaibhavmeena91@gmail.com>
2023-09-11 11:35:32 +0530

    events update

--- src/components/events/EventDetailed.jsx
+++ src/components/events/EventDetailed.jsx
@@ -1,162 +1,144 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import TimeComponent from '../admin/Event/EditTimeFormat';
-import { Button } from "@/components/ui/button"
-import { MapPin, Clock, Calendar } from "lucide-react"
+import React from "react";
+import { useLocation } from "react-router-dom";
+import TimeComponent from "../admin/Event/EditTimeFormat";
+import { Button } from "@/components/ui/button";
+import { MapPin, Clock, Calendar } from "lucide-react";
+import { Separator } from "@/components/ui/separator"
+
 const EventDetails = () => {
-    const location = useLocation();
-    const event = location.state.event;
+  const location = useLocation();
+  const event = location.state.event;
 
-
-    const [openTab, setOpenTab] = React.useState(1);
-
-    return (
-        <div className=' p-8 md:p-16 lg:px-72 '>
-            <h1 className=" font-extrabold font-raleway  text-center pb-4 tracking-tight text-3xl  md:text-5xl">
-                EVENTS
-            </h1>
-
-
-
-
-
-            <div className="w-full border">
-
-                {/* Banner 2 */}
-                {/* <div className="w-full mb-4   rounded-md border dark:border-white border-black ">
+  const [openTab, setOpenTab] = React.useState(1);
+  {
+    /* Banner 2 */
+  }
+  {
+    /* <div className="w-full mb-4   rounded-md border dark:border-white border-black ">
                     <img src={event.banner_url_2} alt="Event Banner" style={{ aspectRatio: "30 / 9" }} className="h-full w-full object-cover  rounded-md" />
-                </div> */}
+                </div> */
+  }
+  return (
+    <div className=" p-8 sm:p-16 md:px-24 lg:36 xl:px-72 ">
+      <h1 className=" font-extrabold font-raleway  text-center pb-4 tracking-tight text-3xl  sm:text-5xl">
+        EVENTS
+      </h1>
 
-                {/* Profile Image with Event details */}
-                <div className="flex  items-center gap-2 mb-4">
+      <div className="w-full border  ">
+        {/* Profile Image with Event details */}
+        <div className="flex flex-col sm:flex-row items-start sm:items-center gap-4 mb-4">
+          {/* Profile Image */}
+          <div className=" sm:flex-none items-center justify-center">
+            <img
+              src={event.banner_url_1}
+              alt="Event Poster"
+              className="w-full  sm:max-h-64 rounded"
+            />
+          </div>
 
-                    {/* Profile Image */}
-                    <div className="flex-none  items-center justify-center flex ">
-                        <img src={event.banner_url_1} alt="Event Poster" className="  max-h-60  rounded" />
-                    </div>
-
-                    {/* Event details beside the image */}
-                    <div className=" w-full   flex flex-col ">
-                    <div className='pb-4'>
-                            <h1 className=" font-raleway font-extrabold text-xl md:text-6xl overflow-clip">{event.event_name}</h1>
-                            <p className="font-raleway opacity-70 text-base md:text-2xl ">{event.society_name}</p>
-                        </div>
-
-                        <div className="flex font-raleway gap-x-4 md:gap-x-6 gap-y-1 flex-wrap ">
-                            <div className="flex w-full md:w-auto items-center md:gap-2">
-                                <MapPin className='h-4 md:h-auto' />
-                                <span className=" text-sm  md:text-2xl">{event.venue} </span>
-                            </div>
-                            <div className="flex items-center gap-1 md:gap-2">
-                                <Calendar className='h-4 md:h-auto' />
-                                <span className="text-sm  md:text-2xl">{event.day_number} </span>
-                            </div>
-                            <div className="flex items-center justify-center gap-1  md:gap-12">
-                                <Clock  className='h-4 md:h-auto' />
-                                <span className="text-sm md:text-2xl  "><TimeComponent timeValue={event.time} /></span>
-                            </div>
-                            
-                        </div>
-                        {/* Registration button on the rightest side */}
-                        <div className="md:block py-4  flex items-center justify-center hidden ">
-                            <Button variant="destructive" className=" font-raleway   w-3/4 sm:text-lg md:text-xl">
-                                <a href={event.registration_link} target="_blank" rel="noopener noreferrer">
-                                    Register
-                                </a>
-                            </Button>
-                        </div>
-                    </div>
-
-
-
+          <div className="  relative w-full">
+            <div>
+              {/* Event details below the image in mobile and beside the image in larger views */}
+              <div className="w-full items-center sm:items-start   flex flex-col  ">
+                <div className=" py-2 sm:py-4">
+                  <h1 className="font-raleway flex justify-center sm:justify-normal font-extrabold text-4xl sm:text-5xl md:text-6xl ">
+                    {event.event_name}
+                  </h1>
+                  <p className="font-raleway flex justify-center sm:justify-normal opacity-70 text-xl sm:text-2xl ">
+                    {event.society_name}
+                  </p>
                 </div>
-                <Button variant="destructive" className="w-full md:hidden font-raleway sm:text-lg md:text-xl">
-                    <a href={event.registration_link} target="_blank" rel="noopener noreferrer">
-                        Register
-                    </a>
-                </Button>
-                {/* Description */}
-                <hi className=" justify-center mt-4 flex font-raleway sm:text-lg md:text-xl" > ABOUT THE EVENT </hi>
-                <h1 className='mb-4  px-4 sm:text-lg md:text-xl'>{event.description}</h1>
-
-
-                {/* Event POC details */}
-                <div>
-                    <h4 className="text-lg font-semibold">Event POCs:</h4>
-                    <div className="space-y-1">
-                        {event.name_poc_1 && event.phone_poc_1 && <p>{event.name_poc_1} - {event.phone_poc_1}</p>}
-                        {event.name_poc_2 && event.phone_poc_2 && <p>{event.name_poc_2} - {event.phone_poc_2}</p>}
-                        {event.name_poc_3 && event.phone_poc_3 && <p>{event.name_poc_3} - {event.phone_poc_3}</p>}
-                    </div>
+                <div className="flex justify-center sm:justify-normal font-raleway gap-x-4 sm:gap-x-6 gap-y-1 flex-wrap ">
+                  <div className="flex items-center gap-1 sm:gap-2">
+                    <Calendar className="h-auto" />
+                    <span className="text-lg  sm:text-2xl">
+                      Day {event.day_number}
+                    </span>
+                  </div>
+                  <div className="flex  gap-1  md:gap-12">
+                    <Clock className="h-auto" />
+                    <span className="text-lg sm:text-2xl  ">
+                      <TimeComponent timeValue={event.time} />
+                    </span>
+                  </div>
+                  <div className="flex sm:w-auto items-center sm:gap-2">
+                    <MapPin className="h-auto" />
+                    <span className=" text-lg  sm:text-2xl">
+                      {event.venue}
+                    </span>
+                  </div>
                 </div>
-
+              </div>
             </div>
-
+            {/* Registration button */}
+            <div className="py-4 scale-95 xl:scale-125  xl:absolute top-1/3 max-w-xl align-middle right-10 sm:flex hidden  items-center justify-center">
+              <Button
+                variant="destructive"
+                className="w-full font-raleway sm:text-xl"
+              >
+                <a
+                  href={event.registration_link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className=" font-raleway"
+                >
+                  Register
+                </a>
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center ">
+          <Button
+            variant="destructive"
+            className="w-2/4  sm:hidden font-raleway sm:text-lg sm:text-xl"
+          >
+            <a
+              href={event.registration_link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-raleway"
+            >
+              Register
+            </a>
+          </Button>
         </div>
 
+        {/* Description */}
+        <hi className=" justify-center underline mt-4 flex font-raleway text-lg sm:text-xl">
+          ABOUT THE EVENT
+        </hi>
 
-    )
-}
+        <h1 className="mb-4 py-2 px-4 sm:px-8 text-md sm:text-xl">
+          {event.description}
+        </h1>
+
+        {/* Event POC details */}
+        <div>
+        <h1 className=" justify-center  mt-4 flex font-raleway text-lg sm:text-xl">
+            EVENT POC
+          </h1>
+          <div className="space-y-1">
+            {event.name_poc_1 && event.phone_poc_1 && (
+              <p>
+                {event.name_poc_1} - {event.phone_poc_1}
+              </p>
+            )}
+            {event.name_poc_2 && event.phone_poc_2 && (
+              <p>
+                {event.name_poc_2} - {event.phone_poc_2}
+              </p>
+            )}
+            {event.name_poc_3 && event.phone_poc_3 && (
+              <p>
+                {event.name_poc_3} - {event.phone_poc_3}
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default EventDetails;
-
-
-
-
-
-
-// import React from 'react'
-// import { Button } from "@/components/ui/button"
-// import EventGrid from "../components/events/eventcomponent";
-
-// export const Events = () => {
-//     const [openTab, setOpenTab] = React.useState(1);
-
-//     return (
-//         <div className=' p-8 md:p-16 lg:px-48 '>
-//              <h1 className=" font-extrabold font-raleway  text-center pb-4 tracking-tight text-3xl  md:text-5xl">
-//                         EVENTS
-//                     </h1>
-//             <ul className="flex w-full list-none flex-wrap border-b-4 border-black dark:border-slate-100 m-0 p-0 text-end space-x-0 items-end">
-//                 <li className={` ${openTab === 1 ? "border-b-4 text-3xl" : "text-slate-500 text-2xl"} border-black   dark:border-slate-100 m-0 p-0`}>
-//                     <div onClick={e => {
-//                         e.preventDefault();
-//                         setOpenTab(1);
-//                     }}>
-//                         <h1 className={`${openTab === 1 ? "md:text-3xl transition-all text-3xl" : "md:text-2xl transition-all hover:text-3xl text-2xl"} md:pb-4 pb-2 font-extrabold tracking-tight  px-4`}>
-//                             DAY 1
-//                         </h1>
-//                     </div>
-//                 </li>
-
-//                 <li className={`  ${openTab === 2 ? "border-b-4 text-3xl" : "text-slate-500 text-2xl"} border-black dark:border-slate-100 m-0 p-0`}>
-//                     <div onClick={e => {
-//                         e.preventDefault();
-//                         setOpenTab(2);
-//                     }}>
-//                         <h1 className={`${openTab === 2 ? "md:text-3xl transition-all text-3xl" : "md:text-2xl  transition-all hover:text-3xl text-2xl"} md:pb-4 pb-2 font-extrabold tracking-tight  px-4`}>
-//                             DAY 2
-//                         </h1>
-//                     </div>
-//                 </li>
-//                 <li className={` flex-1   md:flex-none ${openTab === 3 ? "border-b-4 text-3xl" : "text-slate-500 text-2xl"} border-black dark:border-slate-100 m-0 p-0`}>
-//                     <div onClick={e => {
-//                         e.preventDefault();
-//                         setOpenTab(3);
-//                     }}>
-//                         <h1 className={`${openTab === 3 ? "md:text-3xl transition-all text-3xl" : "md:text-2xl transition-all hover:text-3xl text-2xl"} md:pb-4 pb-2 text-left font-extrabold  px-4 tracking-tightpx-8 `}>
-//                             DAY 3
-//                         </h1>
-//                     </div>
-//                 </li>
-//             </ul>
-
-
-//             <div className="pt-4 md:pt-8">
-//                 <EventGrid openTab={openTab} />
-
-//             </div>
-//         </div>
-
-//     )
-// }
--- src/components/navigation-bar.tsx
+++ src/components/navigation-bar.tsx
@@ -13,32 +13,36 @@ export function NavigationBar() {
   return (
     <div className="flex p-4 justify-between w-full  border-b border-gray-500">
       {/* Left: NSUTTHON */}
-          <div className="relative z-20 flex items-center p-0 m-0 text-lg font-medium">
+      <Link to="/">
+
+          <div className="relative z-20  flex items-center p-0 m-0  ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
+              // strokeWidth="1"
+              // strokeLinecap="round"
+              // strokeLinejoin="round"
               className="mr-2 h-6 w-6"
             >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+            <path d="M23.8222 23.6119L16.9206 13.1154L16.0109 14.5579L20.8404 21.8867C20.9159 22.0013 20.8435 22.1639 20.717 22.1639H16.3907C16.3406 22.1639 16.2936 22.1364 16.2648 22.0901L12.9433 16.7513C12.9057 16.6911 12.9061 16.6106 12.9441 16.5506L23.1892 0.385706C23.2621 0.270851 23.1892 0.111115 23.0642 0.111115H15.3934C15.3453 0.111115 15.3 0.136451 15.2709 0.179722L12.0753 4.93222C12.0132 5.02456 11.89 5.02351 11.829 4.93029L8.72731 0.181652C8.6983 0.137255 8.65241 0.111115 8.60348 0.111115H0.69907L7.8474 11.3323C7.88593 11.3927 7.88598 11.4742 7.84755 11.5347L0.176278 23.6142C0.103384 23.7291 0.176064 23.8889 0.301258 23.8889H8.16858C8.21864 23.8889 8.26566 23.8614 8.29437 23.8151L10.9997 19.4724L10.7963 19.1417L11.0023 19.4683L11.0039 19.471L11.7979 18.2804L11.9359 18.0735L15.5594 23.816C15.5883 23.8618 15.6349 23.8889 15.6847 23.8889H23.6986C23.825 23.8889 23.8975 23.7264 23.8222 23.6119ZM12.8403 6.46034L15.8523 1.84014C15.8813 1.79574 15.9272 1.76944 15.9761 1.76944H20.1877C20.3135 1.76944 20.386 1.9307 20.3118 2.0454L15.2277 9.90028C15.1667 9.99456 15.0423 9.99534 14.9803 9.90173L12.8409 6.66706C12.8004 6.60584 12.8003 6.52184 12.8403 6.46034ZM11.9213 14.8206L10.6287 13.1331L9.66694 14.4775L11.0149 16.6138L10.88 16.8249L10.1741 17.9312L10.1718 17.9275L10.101 18.0392L7.54905 22.0659C7.52019 22.1116 7.47356 22.1386 7.42404 22.1386H3.30706C3.18179 22.1386 3.10911 21.9785 3.18222 21.8637L11.1092 9.4165L10.2301 8.02228L10.22 8.00878L8.92701 9.95111C8.86478 10.0447 8.73997 10.0433 8.67935 9.94839L3.71185 2.17039C3.63853 2.05561 3.71114 1.89531 3.83655 1.89531H8.07327C8.12294 1.89531 8.16947 1.92234 8.19823 1.96786L14.2181 11.4578C14.2569 11.5189 14.2564 11.6013 14.2169 11.6619L12.1614 14.81C12.1029 14.8996 11.9858 14.9048 11.9213 14.8206Z"  fill="#0F0F0F"         className="dark:fill-[#e4eaf0]"/>
             </svg>
-            NSUT THON
+            <h1 className=" font-raleway text-2xl">NSUTTHON </h1>
           </div>
+        </Link>
+
 
       {/* Middle: Leaderboard and Events */}
-      <div className="flex space-x-4 hidden sm:flex">
+      <div className="flex space-x-4 hidden  sm:flex">
         <Link to="/leaderboard">
-          <Button variant="outline" className="w-full" type="button">
+          <Button variant="outline" className="w-full font-raleway" type="button">
             LEADERBOARD
 
           </Button>
         </Link>
         <Link to="/events">
-          <Button variant="outline" className="w-full" type="button">
+          <Button variant="outline" className="w-full  font-raleway" type="button">
             EVENTS
           </Button>
 
@@ -69,14 +73,14 @@ export function NavigationBar() {
 
  */}
         <Link to="/register">
-          <Button variant="outline" className="w-full" type="button">
+          <Button variant="outline" className="w-full font-raleway" type="button">
             REGISTER
           </Button>
         </Link>
        
       {user && (
         <Link className=" hidden md:block" to="/admin/dashboard">
-          <Button variant="outline" className="w-full" type="button">
+          <Button variant="outline" className="w-full font-raleway" type="button">
             DASHBOARD
           </Button>
         </Link>
