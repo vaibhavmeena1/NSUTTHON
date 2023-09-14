8c4aa8c02706464f74d91761b3a869943a4b47e9
Vaibhav Meena <vaibhavmeena91@gmail.com>
2023-09-14 18:18:59 +0530

    transition stop

--- src/components/events/EventImages.jsx
+++ src/components/events/EventImages.jsx
@@ -8,6 +8,8 @@ const ImageScroller = ({ imageLinks }) => {
   const filteredImageLinks = imageLinks.filter(Boolean);
 
   useEffect(() => {
+    if (filteredImageLinks.length <= 1) return; // Don't set interval if there's only one image
+
     const interval = setInterval(() => {
       setTransitioning(true);
       setTimeout(() => {
@@ -21,6 +23,8 @@ const ImageScroller = ({ imageLinks }) => {
 
   const handlers = useSwipeable({
     onSwipedLeft: () => {
+      if (filteredImageLinks.length <= 1) return; // Don't handle swipe if there's only one image
+
       setTransitioning(true);
       setTimeout(() => {
         setCurrentImageIndex((prevIndex) => (prevIndex + 1) % filteredImageLinks.length);
@@ -28,6 +32,8 @@ const ImageScroller = ({ imageLinks }) => {
       }, 300);
     },
     onSwipedRight: () => {
+      if (filteredImageLinks.length <= 1) return; // Don't handle swipe if there's only one image
+
       setTransitioning(true);
       setTimeout(() => {
         setCurrentImageIndex((prevIndex) => (prevIndex - 1 + filteredImageLinks.length) % filteredImageLinks.length);
@@ -46,15 +52,17 @@ const ImageScroller = ({ imageLinks }) => {
         className={`w-full sm:max-h-64 rounded ${transitioning ? 'opacity-0' : 'opacity-100'} transition-opacity duration-300`}
         {...handlers}
       />
-      <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex space-x-2">
-        {filteredImageLinks.map((_, index) => (
-          <span
-            key={index}
-            className={`h-2 w-2 rounded-full cursor-pointer ${index === currentImageIndex ? 'bg-black' : 'bg-gray-400'}`}
-            onClick={() => setCurrentImageIndex(index)}
-          ></span>
-        ))}
-      </div>
+      { filteredImageLinks.length > 1 && (
+        <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex space-x-2">
+          {filteredImageLinks.map((_, index) => (
+            <span
+              key={index}
+              className={`h-2 w-2 rounded-full cursor-pointer ${index === currentImageIndex ? 'bg-black' : 'bg-gray-400'}`}
+              onClick={() => setCurrentImageIndex(index)}
+            ></span>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
