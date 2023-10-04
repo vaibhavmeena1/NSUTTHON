83e5ea33a7b96690993620677a3ace01d02063ea
Vaibhav Meena <vaibhavmeena91@gmail.com>
2023-10-05 03:56:59 +0530

    page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,5 @@
 import { ChevronDown } from "lucide-react";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NsutthonGrid } from "../components/Homepage/Grid";
 import { Faq } from "@/components/Homepage/Faq";
 import { Button } from "@/components/ui/button";
@@ -8,42 +8,114 @@ import { Label } from "@/components/ui/label";
 export function HomePage() {
   const [showAllFaqs, setShowAllFaqs] = useState(false);
 
+  useEffect(() => {
+    // Move the query inside the useEffect
+    const snapContainer = document.querySelector('.snap-container');
+
+    if (!snapContainer) {
+      console.error("snapContainer is null");
+      return;
+    }
+
+    let timeoutId = null;
+    const onScroll = () => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        const currentScroll = snapContainer.scrollTop;
+        const snapHeight = window.innerHeight;
+
+        const snapIndex = Math.round(currentScroll / snapHeight);
+        snapContainer.scrollTo(0, snapIndex * snapHeight);
+      }, 100);
+    };
+
+    snapContainer.addEventListener('scroll', onScroll);
+
+    return () => {
+      snapContainer.removeEventListener('scroll', onScroll);
+    };
+}, []); // The empty dependency array ensures this runs after the first render and not again
+
+
+  const styles = {
+    snapContainer: {
+      height: "100vh",
+      overflowY: "scroll",
+      scrollSnapType: "y mandatory",
+    },
+    section: {
+      scrollSnapAlign: "start",
+    },
+  };
+
   return (
-    <>
-      <section className="pt-1 h-screen    overflow-hidden">
+    <div style={styles.snapContainer}>
+      <section
+        style={styles.section}
+        className="pt-1 h-screen    overflow-hidden"
+      >
         <HeroSection />
       </section>
 
       <div className="md:pt-0 md:p-16 md:pb-0 lg:px-[10vw] xl:px-[12vw] px-[6vw]">
-        <section className="h-screen w-full flex  justify-center items-center flex-col ">
+        <section
+          style={styles.section}
+          className="h-screen w-full flex  justify-center items-center flex-col "
+        >
           <h1 className="font-extrabold mb-14 font-raleway text-center tracking-tight text-2xl md:text-4xl">
-          NSUTTHON: Last Year's Highlights
+            NSUTTHON: Last Year's Highlights
           </h1>
           <div className=" w-full">
-            {" "}
             <NsutthonGrid />
           </div>
         </section>
 
-        <section className="h-screen  w-full flex  flex-col justify-center items-center ">
-          <h1 className="font-extrabold font-raleway text-center pt-16 p-4 tracking-tight text-2xl md:text-4xl">
-            FAQs
-          </h1>
-          <div className="w-full">
-          <Faq showAll={showAllFaqs} />
-
-          </div>
-          <div className="text-center -mt-7 relative">
-            <button
-              className={`transform transition-transform duration-300 p-2 rounded-full ${
-                showAllFaqs ? "rotate-180" : ""
-              } dark:bg-[hsl(0,0%,14.9%)] bg-[hsl(0,0%,89.9%)]`}
-              onClick={() => setShowAllFaqs(!showAllFaqs)}
-            >
-              <ChevronDown className="h-8 w-8 stroke-2" />
-            </button>
-          </div>
-        </section>
+        <section
+  style={styles.section}
+  className="  h-1/2 w-full flex flex-col justify-end items-center "
+>
+  <h1 className="font-extrabold font-raleway text-center pt-16 p-4 tracking-tight text-2xl md:text-4xl">
+    FAQs
+  </h1>
+  <div className="w-full">
+    <Faq showAll={showAllFaqs} />
+  </div>
+  <div className="text-center -mt-7 relative">
+    <button
+      className={`transform transition-transform duration-300 p-2 rounded-full ${
+        showAllFaqs ? "rotate-180" : ""
+      } dark:bg-[hsl(0,0%,14.9%)] bg-[hsl(0,0%,89.9%)]`}
+      onClick={() => setShowAllFaqs(!showAllFaqs)}
+    >
+      <ChevronDown className="h-8 w-8 stroke-2" />
+    </button>
+  </div>
+  <footer className="flex justify-center flex-col sm:justify-end mb-10 text-xs text-gray-500 py-2 opacity-80 font-georgia">
+    <span className="text-right hidden sm:block">
+      © 2023 NSUTTHON. Developed by{" "}
+      <a
+        href="https://www.linkedin.com/in/vaibhavmeena1/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Vaibhav Meena
+      </a>
+      .
+    </span>
+    <div className="sm:hidden text-center">© 2023 NSUTTHON</div>
+    <div className="sm:hidden text-center">
+      Developed by{" "}
+      <a
+        href="https://www.linkedin.com/in/vaibhavmeena1/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Vaibhav Meena
+      </a>
+    </div>
+  </footer>
+</section>
+{/* 
         <footer className="flex justify-center flex-col sm:justify-end text-xs text-gray-500  py-2 opacity-80 font-georgia">
           <span className=" text-right hidden sm:block">
             © 2023 NSUTTHON. Developed by{" "}
@@ -67,8 +139,8 @@ export function HomePage() {
               Vaibhav Meena
             </a>
           </div>
-        </footer>
+        </footer> */}
       </div>
-    </>
+    </div>
   );
 }
