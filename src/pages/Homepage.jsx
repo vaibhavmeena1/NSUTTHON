83f4e72331ebb093a3d22a5e69f95a29dfa24be7
Gaurav Tiwary <97665755+Gaurav-1306@users.noreply.github.com>
2023-10-10 17:38:49 +0530

    overflow problem4

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -53,7 +53,7 @@ export function HomePage() {
       </section>
  <section
         
-        className="min-h-screen w-full overflow-x-visible"
+        className="w-full overflow-x-visible"
       >
         <Events/>
          
